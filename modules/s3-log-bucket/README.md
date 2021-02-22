79e15f4d02471b8be6e047f8649ddbdd2592fb63
Byungjin Park <posquit0.bj@gmail.com>
2021-02-22 11:54:58 +0900

    Add s3-log-bucket module

---
+++ modules/s3-log-bucket/README.md
@@ -0,0 +1,64 @@
+# s3-log-bucket
+
+This module creates following resources.
+
+- `aws_s3_bucket`
+- `aws_s3_bucket_policy`
+- `aws_s3_bucket_public_access_block` (optional)
+
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+## Requirements
+
+| Name | Version |
+|------|---------|
+| terraform | >= 0.13 |
+| aws | >= 3.27 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| aws | >= 3.27 |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| name | Desired name for the VPC Peering resources. | `string` | n/a | yes |
+| cloudfront\_bucket\_prefixes | List of the S3 key prefixes that follows the name of the bucket you have allowed for CloudFront log file delivery. | `list(string)` | `[]` | no |
+| cloudfront\_enabled | Allow CloudFront service to export logs to bucket. | `bool` | `false` | no |
+| cloudfront\_expiration\_date | Specifies the date after which you want the expiration action takes effect for CloudFront prefixes. | `string` | `null` | no |
+| cloudfront\_expiration\_days | Specifies the number of days after object creation when the expiration action takes effect for CloudFront prefixes. | `number` | `365` | no |
+| cloudfront\_expiration\_enabled | A bool that to enable the expiration lifecycle rule for CloudFront prefixes. | `bool` | `false` | no |
+| cloudtrail\_bucket\_prefixes | List of the S3 key prefixes that follows the name of the bucket you have allowed for CloudTrail log file delivery. | `list(string)` | `[]` | no |
+| cloudtrail\_enabled | Allow CloudTrail service to export logs to bucket. | `bool` | `false` | no |
+| cloudtrail\_expiration\_date | Specifies the date after which you want the expiration action takes effect for CloudTrail prefixes. | `string` | `null` | no |
+| cloudtrail\_expiration\_days | Specifies the number of days after object creation when the expiration action takes effect for CloudTrail prefixes. | `number` | `365` | no |
+| cloudtrail\_expiration\_enabled | A bool that to enable the expiration lifecycle rule for CloudTrail prefixes. | `bool` | `false` | no |
+| default\_expiration\_date | Specifies the date after which you want the expiration action takes effect for this bucket. | `string` | `null` | no |
+| default\_expiration\_days | Specifies the number of days after object creation when the expiration action takes effect for this bucket. | `number` | `365` | no |
+| default\_expiration\_enabled | A bool that to enable the expiration lifecycle rule for this bucket. | `bool` | `false` | no |
+| elb\_bucket\_prefixes | List of the S3 key prefixes that follows the name of the bucket you have allowed for ELB(Elastic Load Balancer) log file delivery. | `list(string)` | `[]` | no |
+| elb\_enabled | Allow ELB(Elastic Load Balancer) service to export logs to bucket. | `bool` | `false` | no |
+| elb\_expiration\_date | Specifies the date after which you want the expiration action takes effect for ELB(Elastic Load Balancer) prefixes. | `string` | `null` | no |
+| elb\_expiration\_days | Specifies the number of days after object creation when the expiration action takes effect for ELB(Elastic Load Balancer) prefixes. | `number` | `365` | no |
+| elb\_expiration\_enabled | A bool that to enable the expiration lifecycle rule for ELB(Elastic Load Balancer) prefixes. | `bool` | `false` | no |
+| enforce\_tls | Deny any access to the S3 bucket that is not encrypted in-transit if true. | `bool` | `true` | no |
+| force\_destroy | A bool that indicates all objects (including any locked objects) should be deleted from the bucket so the bucket can be destroyed without error. | `bool` | `false` | no |
+| grants | A list of the ACL policy grant. Conflicts with acl. Valid values for `grant.type` are `CanonicalUser` and `Group`. `AmazonCustomerByEmail` is not supported. Valid values for `grant.permissions` are `READ`, `WRITE`, `READ_ACP`, `WRITE_ACP`, `FULL_CONTROL`. | `list(any)` | `[]` | no |
+| public\_access\_block\_enabled | Enable S3 bucket-level Public Access Block configuration. | `bool` | `false` | no |
+| tags | A map of tags to add to all resources. | `map(string)` | `{}` | no |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| arn | The ARN of the bucket. |
+| domain\_name | The bucket domain name. Will be of format `bucketname.s3.amazonaws.com`. |
+| hosted\_zone\_id | The Route 53 Hosted Zone ID for this bucket's region. |
+| name | The name of the bucket. |
+| policy | The text of the policy. |
+| region | The AWS region this bucket resides in. |
+| regional\_domain\_name | The bucket region-specific domain name. The bucket domain name including the region name. |
+
+<!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
---
+++ modules/s3-log-bucket/main.tf
@@ -0,0 +1,307 @@
+data "aws_caller_identity" "this" {}
+data "aws_canonical_user_id" "this" {}
+data "aws_cloudtrail_service_account" "this" {}
+data "aws_elb_service_account" "this" {}
+
+locals {
+  ## CloudFront
+  cloudfront_canonical_user_id = "c4c1ede66af53448b93c283ce9448c4ba468c9432aa01d700d3878632f77d2d0"
+  cloudfront_bucket_prefixes = [
+    for prefix in var.cloudfront_bucket_prefixes:
+      trim(prefix, "/")
+  ]
+  cloudfront_resources =  [
+    for prefix in local.cloudfront_bucket_prefixes:
+      prefix != ""
+        ? format("%s/%s/*", aws_s3_bucket.this.arn, prefix)
+        : format("%s/*", aws_s3_bucket.this.arn)
+  ]
+
+  ## CloudTrail
+  cloudtrail_bucket_prefixes = [
+    for prefix in var.cloudtrail_bucket_prefixes:
+      trim(prefix, "/")
+  ]
+  cloudtrail_resources =  [
+    for prefix in local.cloudtrail_bucket_prefixes:
+      prefix != ""
+        ? format("%s/%s/*", aws_s3_bucket.this.arn, prefix)
+        : format("%s/*", aws_s3_bucket.this.arn)
+  ]
+
+  ## ELB
+  elb_bucket_prefixes = [
+    for prefix in var.elb_bucket_prefixes:
+      trim(prefix, "/")
+  ]
+  elb_resources =  [
+    for prefix in local.elb_bucket_prefixes:
+      prefix != ""
+        ? format("%s/%s/*", aws_s3_bucket.this.arn, prefix)
+        : format("%s/*", aws_s3_bucket.this.arn)
+  ]
+}
+
+locals {
+  default_grants = [
+    {
+      type        = "CanonicalUser"
+      id          = data.aws_canonical_user_id.this.id
+      permissions = ["FULL_CONTROL"]
+    }
+  ]
+  cloudfront_grant = {
+    type        = "CanonicalUser"
+    id          = local.cloudfront_canonical_user_id
+    permissions = ["FULL_CONTROL"]
+  }
+
+  grants = concat(
+    local.default_grants,
+    var.cloudfront_enabled ? [local.cloudfront_grant] : [],
+    var.grants
+  )
+}
+
+
+###################################################
+# S3 Bucket for AWS Logs
+###################################################
+
+resource "aws_s3_bucket" "this" {
+  bucket        = var.name
+  force_destroy = var.force_destroy
+
+  dynamic "grant" {
+    for_each = length(local.grants) > 1 ? local.grants : []
+
+    content {
+      type = lookup(grant.value, "type", "")
+      id = lookup(grant.value, "id", "")
+      uri = lookup(grant.value, "uri", "")
+      permissions = lookup(grant.value, "permissions", [])
+    }
+  }
+
+  lifecycle_rule {
+    id      = "default-lifecycle"
+    enabled = true
+
+    expiration {
+      date = var.default_expiration_enabled ? var.default_expiration_date : null
+      days = var.default_expiration_enabled ? var.default_expiration_days : 0
+      expired_object_delete_marker = false
+    }
+  }
+
+  dynamic "lifecycle_rule" {
+    for_each = local.cloudfront_bucket_prefixes
+
+    content {
+      id      = "cloudfront-lifecycle-${lifecycle_rule.key}"
+      prefix  = lifecycle_rule.value
+      enabled = true
+
+      expiration {
+        date = var.cloudfront_expiration_enabled ? var.cloudfront_expiration_date : null
+        days = var.cloudfront_expiration_enabled ? var.cloudfront_expiration_days : 0
+        expired_object_delete_marker = false
+      }
+    }
+  }
+
+  dynamic "lifecycle_rule" {
+    for_each = local.cloudtrail_bucket_prefixes
+
+    content {
+      id      = "cloudtrail-lifecycle-${lifecycle_rule.key}"
+      prefix  = lifecycle_rule.value
+      enabled = true
+
+      expiration {
+        date = var.cloudtrail_expiration_enabled ? var.cloudtrail_expiration_date : null
+        days = var.cloudtrail_expiration_enabled ? var.cloudtrail_expiration_days : 0
+        expired_object_delete_marker = false
+      }
+    }
+  }
+
+  dynamic "lifecycle_rule" {
+    for_each = local.elb_bucket_prefixes
+
+    content {
+      id      = "elb-lifecycle-${lifecycle_rule.key}"
+      prefix  = lifecycle_rule.value
+      enabled = true
+
+      expiration {
+        date = var.elb_expiration_enabled ? var.elb_expiration_date : null
+        days = var.elb_expiration_enabled ? var.elb_expiration_days : 0
+        expired_object_delete_marker = false
+      }
+    }
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = merge(
+    {
+      "Name" = var.name
+    },
+    var.tags,
+  )
+}
+
+
+###################################################
+# IAM Policy for S3 Bucket
+###################################################
+
+data "aws_iam_policy_document" "this" {
+  ## CloudTrail
+  dynamic "statement" {
+    for_each = var.cloudtrail_enabled ? ["go"] : []
+
+    content {
+      sid    = "cloudtrail-get-bucket-acl"
+      effect = "Allow"
+      principals {
+        type        = "AWS"
+        identifiers = [data.aws_cloudtrail_service_account.this.arn]
+      }
+      actions   = ["s3:GetBucketAcl"]
+      resources = [aws_s3_bucket.this.arn]
+    }
+  }
+
+  dynamic "statement" {
+    for_each = var.cloudtrail_enabled ? ["go"] : []
+
+    content {
+      sid    = "cloudtrail-put-object"
+      effect = "Allow"
+      principals {
+        type        = "AWS"
+        identifiers = [data.aws_cloudtrail_service_account.this.arn]
+      }
+      actions   = ["s3:PutObject"]
+      resources = local.cloudtrail_resources
+      condition {
+        test     = "StringEquals"
+        variable = "s3:x-amz-acl"
+        values   = ["bucket-owner-full-control"]
+      }
+    }
+  }
+
+  ## ELB
+  dynamic "statement" {
+    for_each = var.elb_enabled ? ["go"] : []
+
+    content {
+      sid    = "elb-get-bucket-acl"
+      effect = "Allow"
+      principals {
+        type        = "Service"
+        identifiers = ["delivery.logs.amazonaws.com"]
+      }
+      actions   = ["s3:GetBucketAcl"]
+      resources = [aws_s3_bucket.this.arn]
+    }
+  }
+
+  dynamic "statement" {
+    for_each = var.elb_enabled ? ["go"] : []
+
+    content {
+      sid    = "elb-put-object"
+      effect = "Allow"
+      principals {
+        type        = "AWS"
+        identifiers = [data.aws_elb_service_account.this.arn]
+      }
+      actions   = ["s3:PutObject"]
+      resources = local.elb_resources
+    }
+  }
+
+  dynamic "statement" {
+    for_each = var.elb_enabled ? ["go"] : []
+
+    content {
+      sid    = "elb-put-object-log-delivery"
+      effect = "Allow"
+      principals {
+        type        = "Service"
+        identifiers = ["delivery.logs.amazonaws.com"]
+      }
+      actions   = ["s3:PutObject"]
+      resources = local.elb_resources
+      condition {
+        test     = "StringEquals"
+        variable = "s3:x-amz-acl"
+        values   = ["bucket-owner-full-control"]
+      }
+    }
+  }
+
+  ## Enforce TLS (HTTPS Only)
+  dynamic "statement" {
+    for_each = var.enforce_tls ? ["go"] : []
+
+    content {
+      sid    = "enforce-tls-requests-only"
+      effect = "Deny"
+      principals {
+        type        = "AWS"
+        identifiers = ["*"]
+      }
+      actions   = ["s3:*"]
+      resources = [
+        aws_s3_bucket.this.arn,
+        "${aws_s3_bucket.this.arn}/*"
+      ]
+      condition {
+        test     = "Bool"
+        variable = "aws:SecureTransport"
+        values   = ["false"]
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.this.json
+}
+
+
+###################################################
+# Public Access Block for S3 Bucket
+###################################################
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  count = var.public_access_block_enabled ? 1 : 0
+  # To avoid OperationAborted: A conflicting conditional operation is currently in progress
+  depends_on = [
+    aws_s3_bucket.this,
+    aws_s3_bucket_policy.this,
+  ]
+
+  bucket = aws_s3_bucket.this.id
+
+  # Block new public ACLs and uploading public objects
+  block_public_acls = true
+  # Retroactively remove public access granted through public ACLs
+  ignore_public_acls = true
+  # Block new public bucket policies
+  block_public_policy = true
+  # Retroactivley block public and cross-account access if bucket has public policies
+  restrict_public_buckets = true
+}
