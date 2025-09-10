e25091cc87d834f66ef5ffe2ec241e133eb1bba5
Byungjin Park (Claud) <posquit0.bj@gmail.com>
2025-09-11 00:22:57 +0900

    feat: improve how to use resource_group in modules (#50)

--- modules/sqs-aws-event-queue/outputs.tf
+++ modules/sqs-aws-event-queue/outputs.tf
@@ -12,3 +12,19 @@ output "arn" {
   description = "The ARN of the SQS queue."
   value       = aws_sqs_queue.this.arn
 }
+
+output "resource_group" {
+  description = "The resource group created to manage resources in this module."
+  value = merge(
+    {
+      enabled = var.resource_group.enabled && var.module_tags_enabled
+    },
+    (var.resource_group.enabled && var.module_tags_enabled
+      ? {
+        arn  = module.resource_group[0].arn
+        name = module.resource_group[0].name
+      }
+      : {}
+    )
+  )
+}
